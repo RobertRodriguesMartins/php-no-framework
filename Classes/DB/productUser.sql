0f8442612c8520de97e049f0df74a1a928e0b901
RobertRodriguesMartins <robert.rm131@gmail.com>
2023-03-09 09:34:23 -0300

    fix: remove productUsder table

--- Classes/DB/productUser.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE productUser (
-    `userId` INT NOT NULL,
-    `productId` INT NOT NULL,
-    `quantity` INT NOT NULL,
-    FOREIGN KEY (userId) REFERENCES users(id),
-    FOREIGN KEY (productId) REFERENCES products(id),
-    PRIMARY KEY (userId, productId)
-);
